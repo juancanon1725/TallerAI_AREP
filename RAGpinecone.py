a3a49053a7978500854a56eddf57b89289b06852
juancanon1725 <98672541+juancanon1725@users.noreply.github.com>
2024-04-15 20:49:05 -0500

    primer commit

---
+++ RAGpinecone.py
@@ -0,0 +1,68 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, PodSpec
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["PINECONE_API_KEY"] = "0f3bf522-4d8b-4e2a-8717-1419f509577e"
+os.environ["PINECONE_ENV"] = "gcp-starter"
+
+def loadText():
+    loader = TextLoader("Conocimiento.txt")
+    documents = loader.load()
+    #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap  = 200,
+        length_function = len,
+        is_separator_regex = False,
+    )
+
+
+    docs = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+
+    import pinecone
+
+
+    index_name = "langchain-demo"
+    pc = Pinecone(api_key='0f3bf522-4d8b-4e2a-8717-1419f509577e')
+
+    print(pc.list_indexes())
+
+    # First, check if our index already exists. If it doesn't, we create it
+    if len(pc.list_indexes())==0:
+        # we create a new index
+        #pc.create_index(name=index_name, metric="cosine", dimension=1536)
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric="cosine",
+            spec=PodSpec(
+                environment=os.getenv("PINECONE_ENV"),
+                pod_type="p1.x1",
+                pods=1
+            )
+        )
+
+    # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
+    docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
+
+def search():
+    embeddings = OpenAIEmbeddings()
+
+    index_name = "langchain-demo"
+    # if you already have an index, you can load it like this
+    docsearch = PineconeVectorStore.from_existing_index(index_name, embeddings)
+
+    query = "Por que el conejo acabó quedándose solo"
+    docs = docsearch.similarity_search(query)
+
+    print(docs[0].page_content)
+
+#loadText()
+search()
